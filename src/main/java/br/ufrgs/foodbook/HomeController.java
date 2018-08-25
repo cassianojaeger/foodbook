78433f9a1d71bf8bdff56669b64f3c86978f97d4
Igor Ferreira <ipferreira@inf.ufrgs.br>
2018-08-24 21:13:34 -0300

    First setup of AngularJS

---
+++ src/main/java/br/ufrgs/foodbook/HomeController.java
@@ -0,0 +1,15 @@
+package br.ufrgs.foodbook;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("project", "Foodbook");
+        return "index";
+    }
+}
