0855166798a57506c1e399fc432ec0204e2f7fb2
Igor Ferreira <ipferreira@inf.ufrgs.br>
2018-09-17 22:18:05 -0300

    Added angular router for app navigation

--- src/main/resources/static/js/app/app.js
+++ src/main/resources/static/js/app/app.js
@@ -1,3 +1,19 @@
 'use strict';
 
-var foodbookApp = angular.module('Foodbook', ['ngMaterial', 'ngMessages', 'ngResource']);
+var foodbookApp = angular.module('Foodbook',
+    [
+        'ngMaterial',
+        'ngMessages',
+        'ngResource',
+        'ngRoute'
+    ]);
+
+foodbookApp.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "js/app/components/mainContent/mainContent.html"
+        })
+        .when("/login", {
+            templateUrl: "js/app/components/login/login.html"
+        });
+});
