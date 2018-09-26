7028fecfd7bf92187b78dc519942857501f84439
Cassiano Stradolini <cassiano.stradolini@sap.com>
2018-09-26 15:52:46 -0300

    ola

---
+++ src/main/java/br/ufrgs/foodbook/configuration/MainConfig.java
@@ -0,0 +1,31 @@
+package br.ufrgs.foodbook.configuration;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+import java.net.URISyntaxException;
+
+@Configuration
+public class MainConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException
+    {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
