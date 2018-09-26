093f7ea902bc7f668c8427d613fa57d82796f345
Cassiano Stradolini <cassiano.stradolini@sap.com>
2018-09-26 16:49:21 -0300

    adding configurations for heroku

--- src/main/java/br/ufrgs/foodbook/configuration/HerokuDataSourceConfig.java
+++ src/main/java/br/ufrgs/foodbook/configuration/HerokuDataSourceConfig.java
@@ -4,16 +4,19 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
-public class MainConfig {
-    @Value("{spring.datasource.driverClassName}") String driverClassName;
+@Profile("heroku")
+public class HerokuDataSourceConfig
+{
+    @Value("${spring.datasource.driverClassName}") String driverClassName;
     @Bean
     public BasicDataSource dataSource()
     {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        String username = System.getenv("JDBC_DATABASE_USERNAME");
-        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+        String dbUrl = System.getenv().get("JDBC_DATABASE_URL");
+        String username = System.getenv().get("JDBC_DATABASE_USERNAME");
+        String password = System.getenv().get("JDBC_DATABASE_PASSWORD");
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
