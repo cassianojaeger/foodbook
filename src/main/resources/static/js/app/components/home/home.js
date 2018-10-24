9e8a94911d70fde0871d7abbf406c82410e2be40
Priscila Rachevsky <priscila.rachevsky@sap.com>
2018-10-24 10:11:01 -0300

    add href at button in home

--- src/main/resources/static/js/app/components/home/home.js
+++ src/main/resources/static/js/app/components/home/home.js
@@ -2,11 +2,4 @@
 
 foodbookApp.controller('HomeController', function ($location) {
     var ctrl = this;
-
-    ctrl.goToCreateGroup = goToCreateGroup;
-
-    function goToCreateGroup() {
-        $location.path("/group/create");
-    };
-
 });
