5aff3334fd1bb8450476eaa987d83a7e589a6962
Igor Ferreira <ipferreira@inf.ufrgs.br>
2018-11-03 08:39:25 -0300

    Updated topbar menu

--- src/main/resources/static/js/app/components/topbar/topbar.js
+++ src/main/resources/static/js/app/components/topbar/topbar.js
@@ -2,13 +2,13 @@
 
 foodbookApp.controller('TopbarController', function () {
     var ctrl = this;
-
 });
 
 foodbookApp.directive('topbar', function () {
     return {
         templateUrl: 'js/app/components/topbar/topbar.html',
         controller: 'TopbarController',
+        controllerAs: 'vm',
         restrict: 'E'
     };
 });
