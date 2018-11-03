e60d45b3b5cb3ae9347200413ab466f81ba0f0c4
Igor Ferreira <ipferreira@inf.ufrgs.br>
2018-11-03 08:02:11 -0300

    Removing edit recipe button when user is not the creator

--- src/main/resources/static/js/app/components/recipe/recipe.js
+++ src/main/resources/static/js/app/components/recipe/recipe.js
@@ -1,9 +1,10 @@
 'use strict';
 
-foodbookApp.controller('RecipeController', function (recipe, group, $scope, TIME) {
+foodbookApp.controller('RecipeController', function (recipe, group, user, $scope, TIME) {
     var vm = $scope;
     recipe.cookTime.timeType = TIME.find(function (time) {return time.code === recipe.cookTime.timeType}).name;
     vm.recipe = recipe;
     vm.group = group;
+    vm.isOwnRecipe = recipe.creator.username === user.name;
 
 });
